f375f1718411675e62765859ea886c6d5fe65522
MilkyMan <tiey5200@gmail.com>
2024-06-03 23:50:22 +0700

    feat: Upload first version

---
+++ src/pages/home.tsx
@@ -0,0 +1,10 @@
+
+function home(){
+    return(
+        <>
+            <h1>Hello Nigga</h1>
+        </>
+    )
+}
+
+export default home
---
+++ src/router/index.tsx
@@ -0,0 +1,13 @@
+import { Outlet, createBrowserRouter } from 'react-router-dom'
+import Home from "../pages/home"
+
+export const router = createBrowserRouter([
+    {
+      path: '/',
+      element: (
+        <div>
+          <Home/>
+        </div>
+      ),
+    },
+])
