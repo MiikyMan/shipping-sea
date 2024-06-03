919546a28d895d567b0c003f4807c3fb5103e11b
MilkyMan <tiey5200@gmail.com>
2024-06-04 00:44:18 +0700

    feat: กระดูกงู

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,9 +1,18 @@
-
+import "./styles/styles.css"
 
 function Navbar(){
     return(
         <>
-            <div className="navbar-container">nigga</div>
+            <div className="navbar-container">
+                <div className="navbar-content">
+                    <div className="navbar-left">Logo</div>
+                    <div className="navbar-right">
+                        <div className="navbar-search">search here</div>
+                        <div className="navbar-cart">cart</div>
+                        <div className="navbar-login">Login</div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
