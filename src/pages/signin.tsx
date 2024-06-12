1dad9a6bcf2ea2f603a250b490d201ecc096b90e
kittikan1650704271 <kittikan.khon@bu.ac.th>
2024-06-12 08:04:03 +0700

    update: sign in ui

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -18,6 +18,8 @@ import Logo from "../components/mockuppics/logo.png";
 import { useState } from 'react';
 import { doSignInWithEmailAndPassword, doSignInWithGoogle } from '../firebase/auth';
 import { useAuth } from '../context/authContext';
+import GoogleIcon from "../components/assets/google.svg"
+import FacebookIcon from "../components/assets/facebook.svg"
 
 const defaultTheme = createTheme();
 
@@ -112,6 +114,20 @@ export default function SignInSide() {
                   setEmail(e.target.value);
                   setErrorMessage('');
                 }}
+                sx={{
+                  '& .MuiOutlinedInput-root': {
+                    '& fieldset': {
+                      borderColor: 'divider',
+                    },
+                    '&:hover fieldset': {
+                      borderColor: '#5AB2FF',
+                    },
+                    '&.Mui-focused fieldset': {
+                      borderColor: '#5AB2FF',
+                      color: '#5AB2FF',
+                    },
+                  },
+                }}
                 value={email}
               />
               <TextField
@@ -142,11 +158,25 @@ export default function SignInSide() {
                     </InputAdornment>
                   ),
                 }}
+                sx={{
+                  '& .MuiOutlinedInput-root': {
+                    '& fieldset': {
+                      borderColor: 'divider',
+                    },
+                    '&:hover fieldset': {
+                      borderColor: '#5AB2FF',
+                    },
+                    '&.Mui-focused fieldset': {
+                      borderColor: '#5AB2FF',
+                      color: '#5AB2FF',
+                    },
+                  },
+                }}
               />
-              <FormControlLabel
+              {/* <FormControlLabel
                 control={<Checkbox value="remember" color="primary" />}
                 label="Remember me"
-              />
+              /> */}
               <Button
                 type="submit"
                 disabled={isSigningIn}
@@ -164,49 +194,73 @@ export default function SignInSide() {
                   }
                 }}
               >
-                {isSigningIn ? 'Signing In...' : 'Sign In'}
+                {isSigningIn ? 'SIGNING IN...' : 'Sign In'}
               </Button>
-              <div style={{ display: 'flex', justifyContent: 'center', marginTop: '10px' }}>
-                <button
-                  disabled={isSigningIn}
-                  onClick={onGoogleSignIn}
-                  className={`google-button`}
-                >
-                  <svg className="w-5 h-5" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <g clipPath="url(#clip0_17_40)">
-                      <path
-                        d="M47.532 24.5528C47.532 22.9214 47.3997 21.2811 47.1175 19.6761H24.48V28.9181H37.4434C36.9055 31.8988 35.177 34.5356 32.6461 36.2111V42.2078H40.3801C44.9217 38.0278 47.532 31.8547 47.532 24.5528Z"
-                        fill="#4285F4"
-                      />
-                      <path
-                        d="M24.48 48.0016C30.9529 48.0016 36.4116 45.8764 40.3888 42.2078L32.6549 36.2111C30.5031 37.675 27.7252 38.5039 24.4888 38.5039C18.2275 38.5039 12.9187 34.2798 11.0139 28.6006H3.03296V34.7825C7.10718 42.8868 15.4056 48.0016 24.48 48.0016Z"
-                        fill="#34A853"
-                      />
-                      <path
-                        d="M11.0051 28.6006C9.99973 25.6199 9.99973 22.3922 11.0051 19.4115V13.2296H3.03298C-0.371021 20.0112 -0.371021 28.0009 3.03298 34.7825L11.0051 28.6006Z"
-                        fill="#FBBC04"
-                      />
-                      <path
-                        d="M24.48 9.49932C27.9016 9.44641 31.2086 10.7339 33.6866 13.0973L40.5387 6.24523C36.2 2.17101 30.4414 -0.068932 24.48 0.00161733C15.4055 0.00161733 7.10718 5.11644 3.03296 13.2296L11.005 19.4115C12.901 13.7235 18.2187 9.49932 24.48 9.49932Z"
-                        fill="#EA4335"
-                      />
-                    </g>
-                    <defs>
-                      <clipPath id="clip0_17_40">
-                        <rect width="48" height="48" fill="white" />
-                      </clipPath>
-                    </defs>
-                  </svg>
-                  {isSigningIn ? 'Signing In...' : 'Continue with Google'}
-                </button>
-              </div>
               <Grid container>
                 <Grid item xs>
                   <Link to="#">
                     Forgot password?
                   </Link>
                 </Grid>
-                <Grid container justifyContent="center" sx={{ mt: 8 }}>
+                <Grid container justifyContent="space-between" sx={{ mt: 1 }}>
+                  <Grid item xs={5}>
+                    <Box sx={{ borderBottom: 1, borderColor: 'divider' ,mt:'10px'}}></Box>
+                  </Grid>
+                  <Grid item xs={2} container justifyContent="center" sx={{color:'divider'}}>
+                    or
+                  </Grid>
+                  <Grid item xs={5}>
+                    <Box sx={{ borderBottom: 1, borderColor: 'divider' ,mt:'10px'}}></Box>
+                  </Grid>
+                </Grid>
+                  <Grid container justifyContent="space-between">
+                    <Button
+                      variant="outlined"
+                      startIcon={<img src={GoogleIcon} alt="Google" />}
+                      size="small"
+                      sx={{
+                        mt: 2,
+                        height: '40px',
+                        width: '200px',
+                        bgcolor: '#ffffff',
+                        borderColor: 'divider',
+                        color: '#202020',
+                        '&:hover': {
+                          bgcolor: '#fafafa',
+                          borderColor: '#5AB2FF',
+                          color: '#5AB2FF',
+                        }
+                      }}
+                      disabled={isSigningIn}
+                      onClick={onGoogleSignIn}
+                      className={`google-button`}
+                    >
+                      {isSigningIn ? 'SIGNING IN...' : 'Google'}
+                    </Button>
+                    <Button
+                      variant="outlined"
+                      startIcon={<img src={FacebookIcon} alt="Facebook" />}
+                      size="small"
+                      bgcolor='#000000'
+                      sx={{
+                        mt: 2,
+                        height: '40px',
+                        width: '200px',
+                        bgcolor: '#ffffff',
+                        borderColor: 'divider',
+                        color: '#202020',
+                        '&:hover': {
+                          bgcolor: '#fafafa',
+                          borderColor: '#5AB2FF',
+                          color: '#5AB2FF',
+                        }
+                      }}
+                      disabled={isSigningIn}
+                    >
+                      {isSigningIn ? 'SIGNING IN...' : 'Facebook'}
+                    </Button>
+                  </Grid>
+                <Grid container justifyContent="center" sx={{ mt: 4 }}>
                   <Typography variant="body2">
                     New to Shipping Sea?
                     <Link to="/signup">
