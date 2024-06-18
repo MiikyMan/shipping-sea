4e369dc70a5bb34fa7634f7bfdd0411c6c676f54
kittikan1650704271 <kittikan.khon@bu.ac.th>
2024-06-18 23:58:51 +0700

    update: ads in storage

--- src/components/ads/ads.tsx
+++ src/components/ads/ads.tsx
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import ads1 from "../mockuppics/niggafriday.png";
-import ads2 from "../mockuppics/cybermonday.png";
-import ads3 from "../mockuppics/limitedtime.png";
 import './ads.css';
+
+const ads = [
+    "https://shippingsea-images.s3.ap-southeast-1.amazonaws.com/ads/niggafriday.png",
+    "https://shippingsea-images.s3.ap-southeast-1.amazonaws.com/ads/limitedtime.png",
+    "https://shippingsea-images.s3.ap-southeast-1.amazonaws.com/ads/cybermonday.png"
+]
+
 
 function Ads() {
     const [slideIndex, setSlideIndex] = useState(1);
@@ -47,23 +51,23 @@ function Ads() {
         <>
             <div className="slideshow-container">
                 <div className="mySlides ads-container">
-                    <img src={ads1} alt="Ad 1" />
+                    <img src={ads[0]} alt="Ad 1" />
                     <div className="prev" onClick={() => plusSlides(-1)}>❮</div>
                     <div className="next" onClick={() => plusSlides(1)}>❯</div>
                 </div>
 
                 <div className="mySlides ads-container">
-                    <img src={ads2} alt="Ad 2" />
+                    <img src={ads[1]} alt="Ad 2" />
                     <div className="prev" onClick={() => plusSlides(-1)}>❮</div>
                     <div className="next" onClick={() => plusSlides(1)}>❯</div>
                 </div>
 
                 <div className="mySlides ads-container">
-                    <img src={ads3} alt="Ad 3" />
+                    <img src={ads[2]} alt="Ad 3" />
                     <div className="prev" onClick={() => plusSlides(-1)}>❮</div>
                     <div className="next" onClick={() => plusSlides(1)}>❯</div>
                 </div>
-                
+
                 <div className="dot-container">
                     <span className="dot" onClick={() => currentSlide(1)}></span>
                     <span className="dot" onClick={() => currentSlide(2)}></span>
