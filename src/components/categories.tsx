8578642c70dbfda25daf4c24ed66d89fb2f68bbc
kittikan1650704271 <kittikan.khon@bu.ac.th>
2024-06-19 02:49:24 +0700

    feat: animation

--- src/components/categories.tsx
+++ src/components/categories.tsx
@@ -6,12 +6,28 @@ import CameraAltIcon from '@mui/icons-material/CameraAlt';
 import HeadphonesIcon from '@mui/icons-material/Headphones';
 import SurroundSoundIcon from '@mui/icons-material/SurroundSound';
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 function Categories(){
 
     return(
         <>
             <div className="categories-container">
+                <motion.div
+                className="box"
+                initial={{ opacity: 0, scale: 0.5 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{
+                    duration: 0.3,
+                    ease: [0, 0.71, 0.2, 1.01],
+                    scale: {
+                    type: "spring",
+                    damping: 7,
+                    stiffness: 100,
+                    restDelta: 0.001
+                    }
+                }}
+                >
                 <div className="category">
                     <Link to={`/category?name=smartphones`} className="category-btn">
                         <IconButton className="category-btn" sx={{ fontSize: 90}}>
@@ -22,6 +38,22 @@ function Categories(){
                         Phone
                     </div>
                 </div>
+                </motion.div>
+                <motion.div
+                className="box"
+                initial={{ opacity: 0, scale: 0.5 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{
+                    duration: 0.3,
+                    ease: [0, 0.71, 0.2, 1.01],
+                    scale: {
+                    type: "spring",
+                    damping: 7,
+                    stiffness: 100,
+                    restDelta: 0.001
+                    }
+                }}
+                >
                 <div className="category">
                     <Link to={`/category?name=electronics`} className="category-btn">
                         <IconButton className="category-btn" sx={{ fontSize: 90}}>
@@ -32,6 +64,22 @@ function Categories(){
                         Electronics
                     </div>
                 </div>
+                </motion.div>
+                <motion.div
+                className="box"
+                initial={{ opacity: 0, scale: 0.5 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{
+                    duration: 0.3,
+                    ease: [0, 0.71, 0.2, 1.01],
+                    scale: {
+                    type: "spring",
+                    damping: 7,
+                    stiffness: 100,
+                    restDelta: 0.001
+                    }
+                }}
+                >
                 <div className="category">
                     <Link to={`/category?name=cameras`} className="category-btn">
                         <IconButton className="category-btn" sx={{ fontSize: 90}}>
@@ -42,6 +90,22 @@ function Categories(){
                         Camera
                     </div>
                 </div>
+                </motion.div>
+                <motion.div
+                className="box"
+                initial={{ opacity: 0, scale: 0.5 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{
+                    duration: 0.3,
+                    ease: [0, 0.71, 0.2, 1.01],
+                    scale: {
+                    type: "spring",
+                    damping: 7,
+                    stiffness: 100,
+                    restDelta: 0.001
+                    }
+                }}
+                >
                 <div className="category">
                     <Link to={`/category?name=headphones`} className="category-btn">
                         <IconButton className="category-btn" sx={{ fontSize: 90}}>
@@ -52,6 +116,22 @@ function Categories(){
                         Headset
                     </div>
                 </div>
+                </motion.div>
+                <motion.div
+                className="box"
+                initial={{ opacity: 0, scale: 0.5 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{
+                    duration: 0.3,
+                    ease: [0, 0.71, 0.2, 1.01],
+                    scale: {
+                    type: "spring",
+                    damping: 7,
+                    stiffness: 100,
+                    restDelta: 0.001
+                    }
+                }}
+                >
                 <div className="category">
                     <Link to={`/category?name=speakers`} className="category-btn">
                         <IconButton className="category-btn" sx={{ fontSize: 90}}>
@@ -62,6 +142,7 @@ function Categories(){
                         Speaker
                     </div>
                 </div>
+                </motion.div>
             </div>
         </>
     )
