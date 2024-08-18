62ba56aabcf1c0a6c48edff5358255e5da4976ff
MilkyMan <tiey5200@gmail.com>
2024-08-19 00:06:10 +0700

    fix: category page footer fixed!

--- src/pages/category.tsx
+++ src/pages/category.tsx
@@ -15,10 +15,10 @@ function category(){
         <>
         <div className="flex flex-col min-h-screen ">
             <Navbar/>
-            <div className="page-container flex-grow p-4 max-md:bg-white max-md:rounded-t-xl relative h-full">
+            <div className="page-container flex-grow p-4 max-md:bg-white max-md:rounded-t-xl relative h-full w-full">
                 <Products categoryName={categoryName}/>
             </div>
-            <div className="bg-white bottom-0 z-0 w-full">
+            <div className="max-md:bg-white bottom-0 z-0 w-full">
                 <Footer/>
             </div>
         </div>
